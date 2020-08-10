1e7417f5c60c140cea29b09bfb4fd2e718d8bb2e
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-10 17:32:55 -0500

    added database=hbnb_test_db and user=hbnb_test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- prepares a MySQL server.
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL  PRIVILEGES ON hbnb_test_db. * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`. * TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
