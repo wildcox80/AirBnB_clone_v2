39e82ce103716511e59ec85ed733184e0995e985
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-11 15:25:42 -0500

    added class method delete and modify class method all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
+    def all(self, cls=None):
+        """Returns the list of objects of one type of class."""
+        if cls:
+            new_dictionary = dict()
+            for obj_key, obj in self.__objects.items():
+                if obj.__class__ == cls:
+                    new_dictionary[obj_key] = obj
+            return new_dictionary
         return FileStorage.__objects
+
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +55,11 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Deletes obj from __objects"""
+        if obj:
+            obj_key = "{}.{}".format(type(obj).__name__, obj.id)
+            if obj_key in FileStorage.__objects:
+                del FileStorage.__objects[obj_key]
+            self.save()
