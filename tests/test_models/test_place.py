abc46bab053f49278f79f45dd1ec191b836a4cb3
Wilder Rincon <1588@holbertonschool.com>
2020-08-13 18:07:17 -0500

    Adding tests folder test_models

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,70 +1,97 @@
 #!/usr/bin/python3
+"""
+Unittest for place
+"""
 
-""" """
-from tests.test_models.test_base_model import test_basemodel
+
 from models.place import Place
+import pep8
+import os
+import unittest
 
 
-class test_Place(test_basemodel):
-    """ """
+class TestPlace(unittest.TestCase):
+    """
+    Unittest for the class Place
+    """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Place"
-        self.value = Place
+    def test_docstring(self):
+        """Checks for docstring"""
+        self.assertTrue(len(Place.__doc__) > 1)
+        self.assertTrue(len(Place.__init__.__doc__) > 1)
+        self.assertTrue(len(Place.__str__.__doc__) > 1)
+        self.assertTrue(len(Place.save.__doc__) > 1)
+        self.assertTrue(len(Place.to_dict.__doc__) > 1)
 
-    def test_city_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.city_id), str)
+    def test_pep8(self):
+        """test pep8 comes back clean"""
+        style = pep8.StyleGuide(quiet=True)
+        result = style.check_files(['models/place.py'])
+        self.assertEqual(result.total_errors, 0, "fix pep8")
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+    def setUp(self):
+        """
+        Setup test
+        """
+        pass
 
-    def test_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_init_arg(self):
+        """pass in arg to new instance"""
+        b1 = Place(23)
+        self.assertEqual(type(b1).__name__, "Place")
+        self.assertFalse(hasattr(b1, "23"))
 
-    def test_description(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.description), str)
+    def test_init_kwarg(self):
+        """Pass kwargs into the instance"""
+        b1 = Place(name="Osaka")
+        self.assertEqual(type(b1).__name__, "Place")
+        self.assertTrue(hasattr(b1, "name"))
+        self.assertFalse(hasattr(b1, "id"))
+        self.assertFalse(hasattr(b1, "created_at"))
+        self.assertFalse(hasattr(b1, "updated_at"))
+        self.assertTrue(hasattr(b1, "__class__"))
 
-    def test_number_rooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_rooms), int)
+    def test_str_method(self):
+        """Tests to see if each method is printing accurately"""
+        b1 = Place()
+        b1printed = b1.__str__()
+        self.assertEqual(b1printed,
+                         "[Place] ({}) {}".format(b1.id, b1.__dict__))
 
-    def test_number_bathrooms(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.number_bathrooms), int)
+    def test_before_to_dict(self):
+        """Tests instances before using to_dict conversion"""
+        b1 = Place()
+        b1_dict = b1.__dict__
+        self.assertEqual(type(b1).__name__, "Place")
+        self.assertTrue(hasattr(b1, '__class__'))
+        self.assertEqual(str(b1.__class__),
+                         "<class 'models.place.Place'>")
+        self.assertTrue(type(b1_dict['created_at']), 'datetime.datetime')
+        self.assertTrue(type(b1_dict['updated_at']), 'datetime.datetime')
+        self.assertTrue(type(b1_dict['id']), 'str')
 
-    def test_max_guest(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.max_guest), int)
+    def test_after_to_dict(self):
+        """Test instances after using to_dict conversion"""
+        my_model = Place()
+        new_model = Place()
+        test_dict = my_model.to_dict()
+        self.assertIsInstance(my_model, Place)
+        self.assertEqual(type(my_model).__name__, "Place")
+        self.assertEqual(test_dict['__class__'], "Place")
+        self.assertTrue(type(test_dict['__class__']), 'str')
+        self.assertTrue(type(test_dict['created_at']), 'str')
+        self.assertTrue(type(test_dict['updated_at']), 'str')
+        self.assertTrue(type(test_dict['id']), 'str')
+        self.assertNotEqual(my_model.id, new_model.id)
 
-    def test_price_by_night(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.price_by_night), int)
+    def test_has_attribute(self):
+        """Tests if the instance of BaseModel has been correctly made"""
+        b1 = Place()
+        self.assertTrue(hasattr(b1, "__init__"))
+        self.assertTrue(hasattr(b1, "created_at"))
+        self.assertTrue(hasattr(b1, "updated_at"))
+        self.assertTrue(hasattr(b1, "id"))
 
-    def test_latitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
 
-    def test_longitude(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.latitude), float)
-
-    def test_amenity_ids(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+if __name__ == '__main__':
+    unittest.main()
