9f218a2f9f016635c2945b155964581afd9c9d51
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-13 12:17:30 -0500

    Added back places relationships

--- models/user.py
+++ models/user.py
@@ -13,5 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    #places = relationship("Place", backref="user", cascade="all, delete")
+    places = relationship("Place", backref="user", cascade="all, delete")
     #reviews = relationship('Review', backref='user', cascade="all, delete")
