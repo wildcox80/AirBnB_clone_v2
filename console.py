c1af2a01c9ad7574417f2218ad94599ef90014a9
Luz Perdomo <lilyperdomo92@gmail.com>
2020-08-12 21:27:32 -0500

    Fixed do_all

--- console.py
+++ console.py
@@ -203,15 +203,12 @@ class HBNBCommand(cmd.Cmd):
 
         if args:
             args = args.split(' ')[0]  # remove possible trailing args
-            if args not in HBNBCommand.classes:
-                print("** class doesn't exist **")
-                return
-            for k, v in storage._FileStorage__objects.items():
-                if k.split('.')[0] == args:
-                    print_list.append(str(v))
+            all_fetched = storage.all(args)
         else:
-            for k, v in storage._FileStorage__objects.items():
-                print_list.append(str(v))
+            all_fetched = storage.all()
+        print(all_fetched.__class__)
+        for v in all_fetched.values():
+            print_list.append(str(v))
 
         print(print_list)
 
