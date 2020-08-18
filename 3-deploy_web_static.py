a63a18dd6f8ebe974e2a50d6041c9f6fb10312a7
Wilder Rincon <1588@holbertonschool.com>
2020-08-17 20:00:55 -0500

    Adding file exercise 3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Distributes an archive to your web servers, using the function do_deploy"""
+from fabric.contrib import files
+from fabric.api import env, put, run, local
+import time
+import os
+
+env.hosts = ['35.231.89.82', '54.226.196.8']
+
+
+def do_pack():
+    """Gerenate tgz."""
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              format(timestamp))
+        return ("versions/web_static_{:s}.tgz".format(timestamp))
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """Function for deploy."""
+    if not os.path.exists(archive_path):
+        return False
+
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
+
+    try:
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current'.format(name))
+        run('ln -s {} /data/web_static/current'.format(dest))
+        return True
+    except:
+        return False
+
+
+def deploy():
+    """Compress and upload files to remote server."""
+    path = do_pack()
+    print(path)
+    if path is None:
+        return False
+
+    return do_deploy(path)
+
+
+deploy()
