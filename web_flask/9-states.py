da8c6f1302def36d38190325258f216414f1bd14
Wilder Rincon <1588@holbertonschool.com>
2020-09-02 14:59:42 -0500

    Adding changes file exercise 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,24 +9,15 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def route1():
-    """Displays an HTML with a list of all States.
-    """
-    state = storage.all("State")
-    return render_template("9-states.html", state=state)
+@app.route('/states', defaults={'state_id': None}, strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id):
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.{}'.format(state_id)
+    return render_template('9-states.html', states=states, state_id=state_id)
 
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def route2(id):
-    """Displays an HTML with info about <id> (only if exists)"""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-
-
-    return render_template("9-states.html")
 
 @app.teardown_appcontext
 def teardown_db(exception):
