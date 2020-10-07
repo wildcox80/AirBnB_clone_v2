cd88ef148b344611cb858414e90f0156d35a58fd
Wilder Rincon <1588@holbertonschool.com>
2020-10-07 16:28:10 -0500

    Update file 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+starts a Flask web app.
 """
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+@app.route("/", strict_slashes=False)
+def hello():
+    """a script that starts a Flask
+    web application"""
+    return "Hello HBNB!"
 
-    return 'Hello HBNB'
-
-
-@app.route("/airbnb-onepage/", strict_slashes=False)
-def route1():
-    """response for route in task0"""
-    return 'Hello HBNB!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000")
